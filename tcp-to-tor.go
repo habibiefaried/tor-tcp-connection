8ccf40db2bf11ef760d0757bd0b710e4e7be51cf
Habibie Faried <habibiefaried@gmail.com>
2022-05-21 09:47:25 +0000

    feat: init success

---
+++ tcp-to-tor.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+
+	"github.com/cretz/bine/process/embedded"
+	"github.com/cretz/bine/tor"
+)
+
+func main() {
+	fmt.Println("Starting and registering onion service, please wait a couple of minutes...")
+	t, err := tor.Start(nil, &tor.StartConf{ProcessCreator: embedded.NewCreator(), DataDir: "./data-dir-tcp-to-tor", EnableNetwork: true})
+	if err != nil {
+		log.Panicf("Unable to start Tor: %v", err)
+	}
+
+	defer t.Close()
+	// Wait at most a minute to start network and get
+	dialCtx, dialCancel := context.WithTimeout(context.Background(), time.Minute)
+	defer dialCancel()
+
+	dialer, err := t.Dialer(dialCtx, nil)
+	if err != nil {
+		log.Panicf("error dialing remote addr", err)
+	}
+
+	fmt.Println("Listening...")
+	listener, err := net.Listen("tcp", ":12345")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("error accepting connection", err)
+			continue
+		}
+
+		go func() {
+			fmt.Println("Connecting...")
+			conn2, err := dialer.Dial("tcp", "ts5gzjclebmkyopbot4dpaaz6koyywy5fkk62t66ctajveukwf7t34yd.onion:5353")
+			if err != nil {
+				log.Println("error dialing remote addr", err)
+				return
+			}
+
+			go io.Copy(conn2, conn)
+			io.Copy(conn, conn2)
+			conn2.Close()
+			conn.Close()
+		}()
+	}
+}
---
+++ tor-to-tcp.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+
+	"github.com/cretz/bine/process/embedded"
+	"github.com/cretz/bine/tor"
+)
+
+func main() {
+	port := 5353
+	fmt.Println("Starting and registering onion service, please wait a couple of minutes...")
+	t, err := tor.Start(nil, &tor.StartConf{ProcessCreator: embedded.NewCreator(), DataDir: "./data-dir-tor-to-tcp", EnableNetwork: true})
+	if err != nil {
+		log.Panicf("Unable to start Tor: %v", err)
+	}
+	defer t.Close()
+	listenCtx, listenCancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer listenCancel()
+	onion, err := t.Listen(listenCtx, &tor.ListenConf{Version3: true, RemotePorts: []int{5353}})
+	if err != nil {
+		log.Panicf("Unable to create onion service: %v", err)
+	}
+	defer onion.Close()
+
+	fmt.Printf("Listening to %v.onion:%v\n", onion.ID, port)
+
+	for {
+		conn, err := onion.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			continue
+		} else {
+			fmt.Printf("Accepted from %v\n", conn.RemoteAddr())
+		}
+		go func() {
+			conn2, err := net.Dial("tcp", "localhost:8080")
+			if err != nil {
+				log.Println("error dialing remote addr", err)
+				return
+			}
+			go io.Copy(conn2, conn)
+			io.Copy(conn, conn2)
+			conn2.Close()
+			conn.Close()
+		}()
+	}
+}
