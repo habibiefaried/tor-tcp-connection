6b25221b13e4d412ada3f6995c0e90202877a0c7
Habibie Faried <habibiefaried@gmail.com>
2022-05-28 14:17:08 +0000

    feat: dns example

--- examples/tcp-to-tor.go
+++ examples/tcp-to-tor.go
@@ -12,8 +12,9 @@ import (
 )
 
 func main() {
+	onionDNSaddress := "pdnshs76a5djmxzb4c5chvzc5kpv6egk3htorh7u4okvcpdcdj5r5bqd.onion:53"
 	fmt.Println("Starting and registering onion service, please wait a couple of minutes...")
-	t, err := tor.Start(nil, &tor.StartConf{DataDir: "data-dir-tcp-to-tor", EnableNetwork: true})
+	t, err := tor.Start(nil, &tor.StartConf{DataDir: "data-dir-tcp-to-tor", EnableNetwork: true, DebugWriter: nil})
 	if err != nil {
 		log.Panicf("Unable to start Tor: %v", err)
 	}
@@ -28,11 +29,24 @@ func main() {
 		log.Panicf("error dialing remote addr", err)
 	}
 
-	fmt.Println("Listening...")
 	listener, err := net.Listen("tcp", ":12345")
 	if err != nil {
 		panic(err)
 	}
+
+	// For testing the DNS resolve via TOR network
+	fmt.Println("Testing and building network...")
+	r := &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+
+			return dialer.Dial("tcp", onionDNSaddress)
+		},
+	}
+	ip, _ := r.LookupHost(context.Background(), "puredns.org")
+
+	fmt.Println("IP resolv: " + ip[0])
+	fmt.Println("Listening...")
 
 	for {
 		conn, err := listener.Accept()
@@ -43,7 +57,7 @@ func main() {
 
 		go func() {
 			fmt.Println("Connecting...")
-			conn2, err := dialer.Dial("tcp", "i4owyifwx2xctzfblsri45ox5uvkcawt3owvbx5wk4hczcyemae6yhad.onion:5353")
+			conn2, err := dialer.Dial("tcp", onionDNSaddress)
 			if err != nil {
 				log.Println("error dialing remote addr", err)
 				return
